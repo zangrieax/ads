9ff0c888b6f55ed668c42ffc7d8b7b14f5ed9987
AndreyAnin <andrey.anin@ru.axept.com>
2026-02-06 12:13:31 +0300

    up

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,33 @@ import '../styles/globals.css'
 
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
+import { Cormorant_Garamond } from 'next/font/google';
+import { Playfair_Display } from 'next/font/google';
+import { Bebas_Neue } from 'next/font/google';
+
+const cormorantGaramond = Cormorant_Garamond({
+  weight: '700',
+  style: ['normal', 'italic'],
+  subsets: ['latin', 'cyrillic'],
+  display: 'swap',
+  variable: '--font-cormorant-garamond',
+});
+
+const playfairDisplay = Playfair_Display({
+  weight: 'variable',
+  style: ['normal', 'italic'],
+  subsets: ['latin', 'cyrillic'],
+  display: 'swap',
+  variable: '--font-playfair-display',
+});
+
+const bebasNeue = Bebas_Neue({
+  weight: '400',
+  style: 'normal',
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-bebas-neue',
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -11,10 +38,10 @@ function MyApp({ Component, pageProps }: AppProps) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
         <meta name="description" content="Эксклюзивная система привлечения клиентов для агентств элитной недвижимости. Монополия на район. Платите только за результат." />
         <title>NOOVERHEADS | Стратегия эксклюзивного присутствия для элитной недвижимости</title>
-        
-
       </Head>
-      <Component {...pageProps} />
+      <main className={`${cormorantGaramond.variable} ${playfairDisplay.variable} ${bebasNeue.variable} font-sans`}>
+        <Component {...pageProps} />
+      </main>
     </>
   )
 }
